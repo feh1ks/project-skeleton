ac98d9745acfc1b264147306b9a4726b66921efe
Feh1ks <feh1ks1ua1@gmail.com>
2016-05-24 23:53:01 +0300

    added gulp

---
+++ gulp/gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp'),                      // Gulp JS
+    concat = require('gulp-concat'),             // Concat filse
+    imagemin = require('gulp-imagemin'),         // Minify images
+    uglify = require('gulp-uglify'),             // Minify JS
+    include = require('gulp-include'),           // HTML Templates
+    csso = require('gulp-csso'),                 // Minify CSS
+    autoprefixer = require('gulp-autoprefixer'), // Gulp autoprefixer
+    connect = require('connect'),                // Webserver
+    copy2 = require('gulp-copy2'),               // Copy files
+    less = require('gulp-less'),                 // Less compiler
+    path = require('path'),                      // Array of paths to be used for @import directives
+    rename = require("gulp-rename");             // Rename files
+
+/*---------------------------------------------------------------------------------*/
+/*----------------------------------- COPY ----------------------------------------*/
+/*---------------------------------------------------------------------------------*/
+gulp.task('copy', function () {
+    var paths = [
+        {src: 'bower_components/jquery/dist/jquery.min.js', dest: 'dist/js/jquery.min.js'},
+        {src: 'bower_components/bootstrap/dist/js/bootstrap.min.js', dest: 'dist/js/bootstrap.min.js'},
+        {src: 'bower_components/bootstrap/dist/fonts/*.*', dest: 'dist/fonts/'}
+    ];
+    return copy2(paths);
+});
+/*---------------------------------------------------------------------------------*/
+/*------------------------------ -- CONCAT JS -------------------------------------*/
+/*---------------------------------------------------------------------------------*/
+gulp.task('concatJs', function() {
+    return gulp.src(['bower_components/folder/pluginOneJS', 'bower_components/folder/pluginTwoJS'])
+        .pipe(concat('plugins.js'))
+        .pipe(gulp.dest('src/js/'));
+});
+/*---------------------------------------------------------------------------------*/
+/*-------------------------------- COMPRESS JS ------------------------------------*/
+/*---------------------------------------------------------------------------------*/
+gulp.task('compressJs', function() {
+    gulp.src('src/js/*.*')
+        .pipe(uglify())
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest('dist/js/'));
+});
+/*---------------------------------------------------------------------------------*/
+/*------------------------------ COMPRESS IMAGES ----------------------------------*/
+/*---------------------------------------------------------------------------------*/
+gulp.task('compressImages', () =>
+    gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+);
+/*---------------------------------------------------------------------------------*/
+/*-------------------------------- LESS TO CSS ------------------------------------*/
+/*---------------------------------------------------------------------------------*/
+gulp.task('less', function() {
+    return gulp.src(['src/less/style.less', 'src/less/bootstrap.less'])
+        .pipe(less())
+        .pipe(autoprefixer({browsers: ['last 4 versions']}))
+        .pipe(csso())
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest('dist/css'))
+});
+/*---------------------------------------------------------------------------------*/
+/*------------------------------- HTML INCLUDES -----------------------------------*/
+/*---------------------------------------------------------------------------------*/
+gulp.task("includes", function() {
+    gulp.src("src/index.html")
+        .pipe(include())
+        .pipe(gulp.dest("dist/"))
+});
+/*---------------------------------------------------------------------------------*/
+/*---------------------------------- DEFAULT --------------------------------------*/
+/*---------------------------------------------------------------------------------*/
+gulp.task('default', function(){
+    gulp.run('copy', 'concatJs', 'compressJs', 'compressImages', 'less', 'includes');
+
+    // Watch
+    gulp.watch("src/**/*", function(event){
+        gulp.run('copy', 'concatJs', 'compressJs', 'compressImages', 'less', 'includes');
+    });
+});
